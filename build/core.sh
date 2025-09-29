854d1a746351d2fb31ca29227fd7a60ae7d7d75a
Yshell.dev <official@yshell.dev>
2025-09-29 11:17:18 +0530

    QC

--- build/core.sh
+++ build/core.sh
@@ -43,41 +43,41 @@ node ./build/core.js
 
 # Commit Submodule
 
-printf "Enter commit message: "
-read commitMessage
+# printf "Enter commit message: "
+# read commitMessage
 
-if [ -z "$commitMessage" ]; then
-  echo "No commit message provided. Aborting publish."
-  exit 1
-fi
+# if [ -z "$commitMessage" ]; then
+#   echo "No commit message provided. Aborting publish."
+#   exit 1
+# fi
 
-coreDir="./core"
+# coreDir="./core"
 
-# Submodule Commit/Push
-echo "\n--- Publishing Submodule ($coreDir) ---"
-cd "$coreDir" || exit 1
+# # Submodule Commit/Push
+# echo "\n--- Publishing Submodule ($coreDir) ---"
+# cd "$coreDir" || exit 1
 
-git add .
-git commit -m "$commitMessage"
-git push
-if [ $? -ne 0 ]; then
-  echo "\n*** FAILED TO PUBLISH CHANGES TO SUBMODULE ***"
-  echo "Check git status, local changes, and core repository remote setup."
-  exit 1
-fi
-cd - >/dev/null # Go back to main repo
+# git add .
+# git commit -m "$commitMessage"
+# git push
+# if [ $? -ne 0 ]; then
+#   echo "\n*** FAILED TO PUBLISH CHANGES TO SUBMODULE ***"
+#   echo "Check git status, local changes, and core repository remote setup."
+#   exit 1
+# fi
+# cd - >/dev/null # Go back to main repo
 
-echo "Submodule pushed successfully."
+# echo "Submodule pushed successfully."
 
-# Main Repo Commit/Push
-echo "\n--- Publishing Main Repository ---"
-git add "$coreDir"
-git commit -m "Core-updated: $commitMessage"
-git push
-if [ $? -ne 0 ]; then
-  echo "\n*** FAILED TO PUBLISH CHANGES TO MAIN REPO ***"
-  echo "Check git status, local changes, and core repository remote setup."
-  exit 1
-fi
+# # Main Repo Commit/Push
+# echo "\n--- Publishing Main Repository ---"
+# git add "$coreDir"
+# git commit -m "Core-updated: $commitMessage"
+# git push
+# if [ $? -ne 0 ]; then
+#   echo "\n*** FAILED TO PUBLISH CHANGES TO MAIN REPO ***"
+#   echo "Check git status, local changes, and core repository remote setup."
+#   exit 1
+# fi
 
-echo "\nChanges have been published to GitHub."
+# echo "\nChanges have been published to GitHub."
