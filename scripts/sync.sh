29a996f80756c4f28ff181b3c17f555ca0cc43db
Yshell.dev <vyshnav.prasad@outlook.com>
2025-12-02 18:09:34 +0530

    Periodic Commit

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -59,8 +59,6 @@ else
   echo "Cleared flavour block in package.json (using sed fallback)"
 fi
 
-# node ./execute void sync
-# xdev void sync
 ./compiler/scripts/live.sh void sync
 
 # List of relative paths to repositories to commit and push
